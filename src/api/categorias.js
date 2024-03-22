1d2f8daae18b554ce949d7770e36de8b7cef72b0
isabellirosa <isabelli.ifc@gmail>
2024-03-22 17:11:26 -0300

    Aplicação do django

--- src/api/categorias.js
+++ src/api/categorias.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 export default class CategoriasApi {
   async buscarTodasAsCategorias() {
-    const { data } = await axios.get("/categorias/");
+    const { data } = await axios.get("http://127.0.0.1:8000/categorias/");
     return data.results;
   }
   async adicionarCategoria(categoria) {
-    const { data } = await axios.post("/categorias/", categoria);
+    const { data } = await axios.post("http://127.0.0.1:8000/categorias/", categoria);
     return data.results;
   }
   async atualizarCategoria(categoria) {
-    const { data } = await axios.put(`/categorias/${categoria.id}/`, categoria);
+    const { data } = await axios.put(`http://127.0.0.1:8000/categorias/${categoria.id}/`, categoria);
     return data.results;
   }
   async excluirCategoria(id) {
-    const { data } = await axios.delete(`/categorias/${id}/`);
+    const { data } = await axios.delete(`http://127.0.0.1:8000/categorias/${id}/`);
     return data.results;
   }
 }
